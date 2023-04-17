3c4c1d5bf3a178baaef017d1491286876bd3ada8
sujaysuresh515 <130961144+sujaysuresh515@users.noreply.github.com>
2023-04-17 11:52:19 +0530

    airthmatic operations

---
+++ airthematics samp1.py
@@ -0,0 +1,14 @@
+a=5000
+b=1000
+c=4000
+add= a+b+c
+print (add)
+sub= a-b
+print (sub)
+
+d=5000
+e=10000
+div=e/d
+print (div)
+mod=e%d
+print (mod)
---
+++ airthematics samp2.py
@@ -0,0 +1,13 @@
+a=50
+output1=a*10
+print (output1)
+
+b=20000
+c=40000
+d=60000
+output2=b+c+d
+print (output2/3)
+
+e=5.65
+output3=e*10000
+print (output3)
