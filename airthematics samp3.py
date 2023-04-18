d2762b43599848706afb6e0828b837f944bc4a40
sujaysuresh515 <130961144+sujaysuresh515@users.noreply.github.com>
2023-04-18 10:56:27 +0530

    break and airthmatics

---
+++ airthematics samp3.py
@@ -0,0 +1,13 @@
+a=75
+b=10
+add=a+b
+print (add)
+sub=a-b
+print(sub)
+mul=a*b
+print (mul)
+div=a/b
+print (div)
+mod=a%b
+print (mod)
+print ("the above operation =",+add+sub+mul+mod+div)
---
+++ airthematics.py
@@ -0,0 +1,13 @@
+a=9000
+b=3000
+add=a+b
+print (add)
+sub=a-b
+print (sub)
+mul= (a*b)
+print (mul)
+div=a/b
+print (div)
+mod= a%b
+print (mod)
+print ("the overall operations =",+add+sub+mul+mod)
